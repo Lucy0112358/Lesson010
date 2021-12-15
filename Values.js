a29b7991d57b653d485d58d26fc21d6e3e8c67fb
“lusinehsahakian@gmail.com“ <lusinehsahakian@gmail.com>
2021-12-15 16:53:45 +0400

    Task 12 compeleted.

---
+++ Values.js
@@ -0,0 +1,10 @@
+let city = {
+  name: "Paris",
+  population: "2,140,526",
+  continent: "Europe",
+};
+let arr = []
+for (item in city){
+arr.push(city[item])
+}
+ console.log(arr)
---
+++ keys.js
@@ -0,0 +1,12 @@
+let city = {
+  name: "Paris",
+  population: "2,140,526",
+  continent: "Europe",
+};
+let arr = []
+for (item in city){
+arr.push(item)
+}
+ console.log(arr)
+
+
